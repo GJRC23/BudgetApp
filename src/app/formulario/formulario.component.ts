caecd4e4a01903bd922469532d7ff0f1c2c43327
GJRC23 <gastuconsoli8@gmail.com>
2023-09-15 17:30:03 -0300

    appTerminada

---
+++ src/app/formulario/formulario.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Egreso } from '../egreso/egreso.model';
+import { EgresoServicio } from '../egreso/egreso.servicio';
+import { Ingreso } from '../ingreso/ingreso.model';
+import { IngresoServicio } from '../ingreso/ingreso.servicio';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent implements OnInit{
+  tipo:string="ingresoOperacion";
+  descripcionInput:string = '';
+  valorInput:number = 0;
+
+
+  constructor (private ingresoServicio:IngresoServicio,
+              private egresoServicio:EgresoServicio) {}
+
+  ngOnInit(){
+      
+  }
+
+  tipoOperacion(evento: Event){
+    this.tipo = (evento.target as HTMLSelectElement).value;
+  }
+
+  agregarValor(){
+    if(this.tipo === "ingresoOperacion")
+      this.ingresoServicio.ingresos.push(new Ingreso(this.descripcionInput, this.valorInput));
+    else
+      this.egresoServicio.egresos.push(new Egreso(this.descripcionInput, this.valorInput))
+  }
+}
